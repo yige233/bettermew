8914475df0d99936481c72c6103e8f572802a848
yige233 <34409561+yige233@users.noreply.github.com>
2021-10-12 12:41:09 +0800

    Add files via upload

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mew enchantment AutoUpdate ver.
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  个人向脚本，随着网页的更新，可能会失效。主要功能：https://mew.fun/Dove_yige/thoughts/69082567613583360
 // @author       破损的鞘翅
 // @match        https://mew.fun/n/*
@@ -9,15 +9,14 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-
     // Your code here...
     //该脚本为使用cdn自动更新的脚本，有更新会自动实装。脚本仓库：https://github.com/yige233/bettermew
     window.onload = function () {
         var script = document.createElement("script");
-        script.src = "https://cdn.jsdelivr.net/gh/yige233/bettermew/mew.js";
+        script.src = "https://cdn.jsdelivr.net/gh/yige233/bettermew@latest/mew.js";
         document.head.appendChild(script);
-    }
+    };
 
 })();
