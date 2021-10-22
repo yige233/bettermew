52b2a04c7c5bf7e3aa976001e97f181ecd71f70d
一个，一个人 <53390868+yige-yigeren@users.noreply.github.com>
2021-10-22 14:11:55 +0800

    Update 0.32
    修了几个小bug，主要包括：
    把某个忘打的空格补上了
    更新了刷新cdn的提示
    更新了刷新cdn时同时自动刷新页面的功能
    另外挂上了Greasy Fork，因为不想做麻烦的主脚本更新了，挂上后在GreasyFork下载的会自动更新（大概），地址：https://greasyfork.org/zh-CN/scripts/434289-mew-enchantment-autoupdate-ver

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name         Mew enchantment AutoUpdate ver.
 // @namespace    http://tampermonkey.net/
-//@homepage     https://github.com/yige233/bettermew
-// @version      0.31
+// @homepage     https://github.com/yige233/bettermew
+// @version      0.32
 // @description  个人向脚本，随着网页的更新，可能会失效。主要功能：https://mew.fun/Dove_yige/thoughts/69082567613583360
 // @author       破损的鞘翅
 // @match        https://mew.fun/n/*
 // @match        https://mew.fun/sector-explore
 // @icon         https://mew.fun/favicon.png
 // @supportURL   https://github.com/yige233/bettermew/issues
-// @updateURL    https://cdn.jsdelivr.net/gh/yige233/bettermew@latest/tampermonkey.js
 // @grant        GM.registerMenuCommand
 // @grant        GM.getValue
 // @grant        GM.setValue
@@ -22,6 +21,7 @@
         let is_bata = await GM.getValue("is_bata", false),
             script_url = (is_bata) ? "https://pc.doveyige.top/mew/mew.js" : "https://cdn.jsdelivr.net/gh/yige233/bettermew@latest/mew.js",
             bate_notice = (is_bata) ? "使用正式版" : "体验Beta版",
+            Refresh_notice = (is_bata) ? "刷新cdn缓存-正式版" : "刷新cdn缓存-测试版(暂未开放)",
             script = document.createElement("script");
         script.src = script_url;
         document.head.appendChild(script);
@@ -29,15 +29,13 @@
             localStorage.setItem("settings", "");
             alert("已经清除所有设置！")
         });
-        GM.registerMenuCommand("刷新cdn缓存-仅正式版", async () => {
+        GM.registerMenuCommand("Refresh_notice", async () => {
             let path = "/gh/yige233/bettermew@latest/mew.js";
             let res = await fetch(`https://purge.jsdelivr.net${path}`).then(res => res.json()).then(json => {
                 if (json.status != "finished") return "cdn刷新未完成！";
                 if (json.paths[path].throttled) return `cdn刷新冷却中，剩余时间：${json.paths[path].throttlingReset}秒`;
-                alert(`cdn刷新完成，完成于${new Date(json.timestamp).toLocaleString("chinese", {hour12: false})}，请按下Ctrl+F5刷新浏览器缓存。`);
-                return "如果仍然无法更新到新版本，请等待cdn自然刷新。这最多需要24小时。";
-            });
-            alert(res);
+                alert(`刷新完成，完成于${new Date(json.timestamp).toLocaleString("chinese", {hour12: false})}，如果仍然无法更新到新版本，请等待cdn自然刷新。这最多需要24小时。点击确定以刷新页面`)
+            window.location.reload(true);
         });
         GM.registerMenuCommand(bate_notice, () => {
             (is_bata) ? GM.setValue("is_bata", false): GM.setValue("is_bata", true);
