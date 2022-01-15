ffc6a0166b85b243b00f5fa065a7e42b45b1f3d7
yige233 <34409561+yige233@users.noreply.github.com>
2022-01-15 16:01:01 +0800

    Add files via upload

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -2,7 +2,7 @@
 // @name         Mew enchantment AutoUpdate ver.
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/yige233/bettermew
-// @version      0.32
+// @version      0.33
 // @description  个人向脚本，随着网页的更新，可能会失效。主要功能：https://mew.fun/Dove_yige/thoughts/69082567613583360
 // @author       破损的鞘翅
 // @match        https://mew.fun/n/*
@@ -21,13 +21,23 @@
     //该脚本为使用cdn自动更新的脚本，有更新会自动实装。脚本仓库：https://github.com/yige233/bettermew
     window.onload = async function () {
         let is_bata = await GM.getValue("is_bata", false),
-            script_url = (is_bata) ? "https://pc.doveyige.top/mew/mew.js" : "https://cdn.jsdelivr.net/gh/yige233/bettermew@latest/mew.js",
-            bate_notice = (is_bata) ? "使用正式版" : "体验Beta版",
+            update_url = `https://api.mew.fun/api/v1/users/68907366539980800`,
+            announce = await fetch(update_url).then(res => res.json()).then(text => JSON.parse(text.description.split("\n")[1])),
+            script_url = (is_bata) ? "https://pc.doveyige.top/mew/mew.js" : `https://cdn.jsdelivr.net/gh/yige233/bettermew@${announce.hash}/mew.js`,
+            beta_notice = (is_bata) ? "使用正式版" : "体验Beta版",
             script = document.createElement("script");
         script.src = script_url;
-        document.head.appendChild(script);
+        document.head.append(script);
+        let whatsnew = [`当前脚本版本：${announce.ver}`, "更新内容：", ...announce.whatsnew];
+        console.clear();
+        if (await GM.getValue("version") != announce.ver) {
+            GM.setValue("version", announce.ver);
+            for (let i in whatsnew) {
+                console.log(`%c${whatsnew[i]}`, "color: rgb(125 125 125);font-size:16px")
+            };
+        };
         GM.registerMenuCommand("清除所有设置", () => {
-            localStorage.setItem("settings", "");
+            localStorage.setItem("bettermew", "{}");
             alert("已经清除所有设置！")
         });
         if (!is_bata) {
@@ -36,14 +46,14 @@
                 let res = await fetch(`https://purge.jsdelivr.net${path}`).then(res => res.json()).then(json => {
                     if (json.status != "finished") return "cdn刷新未完成！";
                     if (json.paths[path].throttled) return `cdn刷新冷却中，剩余时间：${json.paths[path].throttlingReset}秒`;
-                    alert(`cdn刷新完成于${new Date(json.timestamp).toLocaleString("chinese", {hour12: false})}，请按下Ctrl+F5刷新浏览器缓存。`);
+                    alert(`cdn刷新完成于${new Date(json.timestamp).toLocaleString("chinese", { hour12: false })}，请按下Ctrl+F5刷新浏览器缓存。`);
                     return "如果仍然无法更新到新版本，请等待cdn自然刷新。这最多需要24小时。";
                 });
                 alert(res);
             });
         };
-        GM.registerMenuCommand(bate_notice, () => {
-            (is_bata) ? GM.setValue("is_bata", false): GM.setValue("is_bata", true);
+        GM.registerMenuCommand(beta_notice, () => {
+            (is_bata) ? GM.setValue("is_bata", false) : GM.setValue("is_bata", true);
             window.location.reload(true);
         });
     };
