0de0ffb30d1196bc4478a2b743f602ab6a53fc28
yige233 <34409561+yige233@users.noreply.github.com>
2022-05-24 12:32:12 +0800

    更换cdn链接

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -2,7 +2,7 @@
 // @name         Mew enchantment AutoUpdate ver.
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/yige233/bettermew
-// @version      0.36
+// @version      0.37
 // @description  个人向脚本，随着网页的更新，可能会失效。主要功能：https://mew.fun/Dove_yige/thoughts/69082567613583360
 // @author       破损的鞘翅
 // @match        https://*.mew.fun/n/*
@@ -10,8 +10,8 @@
 // @match        https://*.mew.fun/sector-explore*
 // @icon         https://mew.fun/favicon.png
 // @supportURL   https://github.com/yige233/bettermew/issues
-// @updateURL    https://cdn.jsdelivr.net/gh/yige233/bettermew@latest/tampermonkey.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/yige233/bettermew@latest/tampermonkey.user.js
+// @updateURL    https://fastly.jsdelivr.net/gh/yige233/bettermew@latest/tampermonkey.user.js
+// @downloadURL  https://fastly.jsdelivr.net/gh/yige233/bettermew@latest/tampermonkey.user.js
 // @grant        GM.registerMenuCommand
 // @grant        GM.getValue
 // @grant        GM.setValue
@@ -24,7 +24,7 @@
         let is_bata = await GM.getValue("is_bata", false),
             update_url = `https://api.mew.fun/api/v1/users/68907366539980800`,
             announce = await fetch(update_url).then(res => res.json()).then(text => JSON.parse(text.description.split("\n")[1])),
-            script_url = (is_bata) ? "https://pc.doveyige.top/mew/mew.body.js" : `https://cdn.jsdelivr.net/gh/yige233/bettermew@${announce.hash}/mew.body.js`,
+            script_url = (is_bata) ? "https://pc.doveyige.top/mew/mew.body.js" : `https://fastly.jsdelivr.net/gh/yige233/bettermew@${announce.hash}/mew.body.js`,
             beta_notice = (is_bata) ? "使用正式版" : "体验Beta版",
             script = document.createElement("script");
         script.src = script_url;
@@ -43,7 +43,7 @@
             alert("已经清除所有设置！");
         });
         GM.registerMenuCommand(beta_notice, () => {
-            (is_bata) ? GM.setValue("is_bata", false) : GM.setValue("is_bata", true);
+            (is_bata) ? GM.setValue("is_bata", false): GM.setValue("is_bata", true);
             window.location.reload(true);
         });
     };
